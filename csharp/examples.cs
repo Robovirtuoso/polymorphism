0f6d80cc15712731a1edecdf7aac48d4d209109e
Alex Williams <alexwilliams4@me.com>
2014-04-14 16:59:28 -0500

    example file

--- csharp/examples.cs
+++ csharp/examples.cs
@@ -1,16 +1,17 @@
-"../introduction.yml'
+"../introduction.yml"
 
-"lightsabers/if_else.cs'
-"lightsabers/case.cs'
-"lightsabers/refactor.cs'
-"lightsabers/polymorphism.cs'
+"lightsabers/if_else.cs"
+"lightsabers/case.cs"
+"lightsabers/refactor.cs"
+"lightsabers/polymorphism.cs"
 
-"council/if_else.cs'
-"council/type_checking.cs'
-"council/polymorphism.cs'
-"council/controller.cs'
+"council/if_else.cs"
+"council/type_checking.cs"
+"council/polymorphism.cs"
+"council/controller.cs"
 
-"definitions/ad_hoc'
-"definitions/duck_typing'
+"definitions/ad_hoc.cs"
+"definitions/duck_typing.cs"
+"definitions/smalltalk.cs"
 
-"benefits.cs'
+"benefits.cs"
